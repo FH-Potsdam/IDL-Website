86687eddb50d8759eb645b47b874551eba5dc80c
Sebastian Meier <contact@sebastianmeier.eu>
2022-01-19 20:41:34 +0100

    original images

--- _migration/index.js
+++ _migration/index.js
@@ -14,6 +14,7 @@ let jsonObj = parser.parse(file);
 const publications = [];
 const people = [];
 const projects = [];
+const attachments = [];
 
 const extractText = (txt) => {
   const regex = /<!--:[a-z]*-->[^<]*/gm;
@@ -97,7 +98,7 @@ jsonObj.rss.channel.item.forEach(item => {
       title: extractText(item.title),
       wp_id: item['wp:post_id'],
       page: extractText(item['content:encoded']),
-      slug: item.post_name,
+      slug: item['wp:post_name'],
       year: item.category['#text'],
       authors: {
         names: [],
@@ -121,7 +122,7 @@ jsonObj.rss.channel.item.forEach(item => {
       page: extractText(item['content:encoded']),
       excerpt: extractText(item['excerpt:encoded']),
       wp_id: item['wp:post_id'],
-      slug: item.post_name,
+      slug: item['wp:post_name'],
       group: item.category ? item.category['#text'] : null,
       subtitle: {de:null, en: null},
       website: null,
@@ -145,9 +146,81 @@ jsonObj.rss.channel.item.forEach(item => {
 
     people.push(person);
 
+  // projects
+  } else if (item['wp:post_type'] === 'project') {
+    if (item.category && ('#text' in item.category)) {
+      item.category = [item.category];
+    }
+    let project = {
+      name: extractText(item.title),
+      page: extractText(item['content:encoded']),
+      excerpt: extractText(item['excerpt:encoded']),
+      wp_id: item['wp:post_id'],
+      slug: item['wp:post_name'],
+      group: item.category ? item.category.map(c => c['#text']) : null,
+      subtitle: {de:null, en: null},
+      project_year: null,
+      project_partners_name: [],
+      project_partners_url: [],
+      project_team: null,
+      project_website: null,
+      project_team_external_name: [],
+      project_team_external_url: [],
+      project_material: null,
+      related_publications: null,
+      related_projects: null,
+      thumbnail_id: null,
+      project_funding_name: [],
+      project_funding_url: [],
+      project_funding_logo: [],
+      featured_home: null,
+      project_client_name: [],
+      project_client_url: [],
+      project_client_logo: []
+    };
 
-}
+    project = {...project, ...extractMeta(item['wp:postmeta'], [
+      [/subtitle/gm, 'subtitle'],
+      [/project_year/gm, 'project_year'],
+      [/project_partners_[0-9]*_name/gm, 'project_partners_name', true],
+      [/project_partners_[0-9]*_url/gm, 'project_partners_url', true],
+      [/project_team/gm, 'project_team'],
+      [/project_website/gm, 'project_website'],
+      [/project_team_external_[0-9]*_name/gm, 'project_team_external_name', true],
+      [/project_team_external_[0-9]*_url/gm, 'project_team_external_url', true],
+      [/project_material/gm, 'project_material'],
+      [/related_publications/gm, 'related_publications'],
+      [/related_projects/gm, 'related_projects'],
+      [/_thumbnail_id/gm, 'thumbnail_id'],
+      [/project_funding_[0-9]*_name/gm, 'project_funding_name', true],
+      [/project_funding_[0-9]*_url/gm, 'project_funding_url', true],
+      [/project_funding_[0-9]*_logo/gm, 'project_funding_logo', true],
+      [/featured_home/gm, 'featured_home'],
+      [/project_client_[0-9]*_name/gm, 'project_client_name', true],
+      [/project_client_[0-9]*_url/gm, 'project_client_url', true],
+      [/project_client_[0-9]*_logo/gm, 'project_client_logo', true],
+    ])};
+
+    project.subtitle = extractText(project.subtitle);
+
+    projects.push(project);
+
+    // attachments
+    } else if (item['wp:post_type'] === 'attachment') {
+      let attachment = {
+        name: extractText(item.title),
+        guid: item.guid['#text'],
+        url: item['wp:attachment_url'],
+        wp_id: item['wp:post_id'],
+        link: item['link'],
+        slug: item['wp:post_name']
+      };
+
+      attachments.push(attachment);
+    }
 });
 
 fs.writeFileSync('../src/site/_data/publications.json', JSON.stringify(publications, null, 2), 'utf8');
 fs.writeFileSync('../src/site/_data/people.json', JSON.stringify(people, null, 2), 'utf8');
+fs.writeFileSync('../src/site/_data/projects.json', JSON.stringify(projects, null, 2), 'utf8');
+fs.writeFileSync('../src/site/_data/attachments.json', JSON.stringify(attachments, null, 2), 'utf8');
