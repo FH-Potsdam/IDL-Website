e716e89b6888de2b65fcd7bf3473aea1ca404730
Sebastian Meier <contact@sebastianmeier.eu>
2022-07-11 21:14:22 +0200

    project's publications

---
+++ _migration/acf.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const cleanUp = {
+  "people": ["publications"],
+  "projects": ["related_publications"]
+};
+
+const regEx = /\"(\d*?)\"/gm;
+
+const files = Object.keys(cleanUp);
+for (let f = 0; f < files.length; f += 1) {
+  const file = JSON.parse(fs.readFileSync('../src/site/_data/' + files[f] + '.json', 'utf-8'));
+  file.forEach(item => {
+    cleanUp[files[f]].forEach(key => {
+      item[key] = [...item[key].matchAll(regEx)].map(i => parseInt(i[1]));
+    });
+  });
+  fs.writeFileSync('../src/site/_data/' + files[f] + '-fix.json', JSON.stringify(file), 'utf-8');
+}
