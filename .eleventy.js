8cf2ef6cf8af1178ff7961bdfa616abe4d5508c0
Sebastian Meier <contact@sebastianmeier.eu>
2022-09-06 22:00:28 +0200

    admin integration

--- .eleventy.js
+++ .eleventy.js
@@ -54,7 +54,7 @@ module.exports = function(config) {
   // pass some assets right through
   config.addPassthroughCopy("./src/site/images");
   config.addPassthroughCopy("./src/site/fonts");
-
+  config.addPassthroughCopy("./src/site/admin");
 
   // make the seed target act like prod
   env = (env=="seed") ? "prod" : env;
